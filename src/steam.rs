6a05405bc7a07c858572387d359d47fd62153abb
krista-chan <qbotdev84@gmail.com>
2022-06-11 17:31:40 +0100

    Clean up steam.rs

--- src/steam.rs
+++ src/steam.rs
@@ -1,15 +1,11 @@
-
-    use steamworks::AppId;
-    use steamworks::Client;
-    use steamworks::SingleClient;
-    use steamworks::FriendFlags;
-    use steamworks::PersonaStateChange;
-    pub fn steam_init() -> (Client, SingleClient) {
-        let appid = 1456420;
-        let (client, single) = Client::init_app(appid).unwrap();
-        let _cb = client.register_callback(|p: PersonaStateChange| {
-            println!("Got callback: {:?}", p);
-        });
-       (client, single)
-       
-    }
+use steamworks::Client;
+use steamworks::PersonaStateChange;
+use steamworks::SingleClient;
+pub fn steam_init() -> (Client, SingleClient) {
+    let appid = 1456420;
+    let (client, single) = Client::init_app(appid).unwrap();
+    let _cb = client.register_callback(|p: PersonaStateChange| {
+        println!("Got callback: {:?}", p);
+    });
+    (client, single)
+}
